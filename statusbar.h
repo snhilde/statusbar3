292a94280767e69828cb6216758a387a3be1fe6f
Hilde N <snhilde@protonmail.com>
2019-04-25 19:53:05 -0400

    add DELIMITER to enum list, remove configs_t struct, begin adding comments to sb_routine_t struct, add comments for sb_flags_active and routine_array (renamed from proc_arr)

--- statusbar.h
+++ statusbar.h
@@ -5,7 +5,7 @@
 #include <string.h>
 
 
-enum sb_process_e {
+enum sb_routine_e {
 	BACKUP     = 0,
 	BATTERY    = 1,
 	BRIGHTNESS = 2,
@@ -22,25 +22,28 @@ enum sb_process_e {
 	VOLUME     = 13,
 	WEATHER    = 14,
 	WIFI       = 15,
-	ENDOFLIST  = -1 /* terminator for configs array */
+	DELIMITER  = 100,
+	ENDOFLIST  = -1 /* terminator value */
 };
 
-typedef struct configs_t {
-	enum sb_process_e process;
-	int               bar;
-}configs_t;
+typedef struct sb_routine {
+	enum sb_routine_e    routine;     /* The number assigned to each routine. This is used to
+	                                     access the routine's flags and to match it to various
+										 checks and calls. */
+	char                 output[256]; /*  */
+	size_t               length;      /*  */
+	pthread_t            thread;      /*  */
+	pthread_mutex_t      mutex;       /*  */
+	void *(*thread_func)(void *);     /*  */
+	struct sb_routine *next;          /*  */
+} sb_routine_t;
 
-typedef struct sb_process_t {
-	enum sb_process_e    process;
-	unsigned int         bar;
-	char                 output[256];
-	size_t               length;
-	pthread_t            thread;
-	pthread_mutex_t      mutex;
-	void *(*thread_func)(void *);
-	struct sb_process_t *next;
-} sb_process_t;
+/* These are all the flags for the routine. Because it is global, it is zero'd out on
+ * startup. If a user does not chose a particular routine in the config file, that routine's
+ * flag will remain zero. If the routine is chosen, then its bit flag will be set to 1. */
+unsigned long sb_flags_active;
 
-unsigned long sb_flags_active = 0;
-
-sb_process_t proc_arr[16];
+/* This array will hold all the routine objects. Because it is global, it is zero'd out on
+ * startup. If a user does not chose a particular routine in the config file, that
+ * routine's index will remain empty. */
+sb_routine_t routine_array[16];
