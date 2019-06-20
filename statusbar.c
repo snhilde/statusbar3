5b5aade4fa1a3e964731b679a882d040e3cb4b8f
Hilde N <snhilde@protonmail.com>
2019-06-20 07:20:31 -0400

    temp: make file descriptor

--- statusbar.c
+++ statusbar.c
@@ -265,6 +265,7 @@ static void *sb_cpu_temp_routine(void *thunk)
 	SB_TIMER_VARS;
 	sb_routine_t *routine = thunk;
 	const char   *base    = "/sys/class/hwmon";
+	FILE         *fd;
 
 	routine->skip = SB_FALSE;
 	while(1) {
