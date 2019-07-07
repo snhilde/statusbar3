54ce4a3d800fe161aa3a490a3860c382d72c4dd1
Hilde N <snhilde@protonmail.com>
2019-07-06 21:22:49 -0400

    fan: destroy mutex when thread exits

--- statusbar.c
+++ statusbar.c
@@ -670,6 +670,8 @@ static void *sb_fan_routine(void *thunk)
 		SB_SLEEP;
 	}
 
+	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
+		fprintf(stderr, "Fan routine: Failed to destroy mutex\n");
 	if (fd != NULL)
 		fclose(fd);
 #endif
