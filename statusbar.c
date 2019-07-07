0052e79e2f101a4e9a37519de98136baef058d6a
Hilde N <snhilde@protonmail.com>
2019-07-06 21:21:53 -0400

    battery: destroy mutex when thread exits

--- statusbar.c
+++ statusbar.c
@@ -257,6 +257,8 @@ static void *sb_battery_routine(void *thunk)
 		fclose(fd);
 #endif
 
+	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
+		fprintf(stderr, "Battery routine: Failed to destroy mutex\n");
 	routine->skip = SB_TRUE;
 	return NULL;
 }
