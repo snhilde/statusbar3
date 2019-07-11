308ed195a8654c0f42d826f7711d815c686416c3
Hilde N <snhilde@protonmail.com>
2019-07-11 07:48:13 -0400

    weather: use SB_PRINT_ERROR

--- statusbar.c
+++ statusbar.c
@@ -971,7 +971,7 @@ static void *sb_weather_routine(void *thunk)
 #endif
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
-		fprintf(stderr, "Weather routine: Failed to destroy mutex\n");
+		SB_PRINT_ERROR("Failed to destroy mutex");
 	routine->print = SB_FALSE;
 	return NULL;
 }
