cc6254a53c5a0a9c7e9cc0749fe54b871a71d078
Hilde N <snhilde@protonmail.com>
2019-07-11 07:42:36 -0400

    cpu usage: use SB_PRINT_ERROR

--- statusbar.c
+++ statusbar.c
@@ -307,7 +307,7 @@ static void *sb_cpu_usage_routine(void *thunk)
 #endif
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
-		fprintf(stderr, "CPU Usage routine: Failed to destroy mutex\n");
+		SB_PRINT_ERROR("Failed to destroy mutex");
 	routine->print = SB_FALSE;
 	return NULL;
 }
