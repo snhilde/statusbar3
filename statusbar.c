483bbba9ac8b9d431174d8995627ae874e730af2
Hilde N <snhilde@protonmail.com>
2019-07-11 07:41:07 -0400

    battery: use SB_PRINT_ERROR

--- statusbar.c
+++ statusbar.c
@@ -164,7 +164,7 @@ static void *sb_battery_routine(void *thunk)
 #endif
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
-		fprintf(stderr, "Battery routine: Failed to destroy mutex\n");
+		SB_PRINT_ERROR("Failed to destroy mutex");
 	routine->print = SB_FALSE;
 	return NULL;
 }
