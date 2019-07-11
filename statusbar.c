6074245cfede6ef8d9c460694b82230dc538dccf
Hilde N <snhilde@protonmail.com>
2019-07-11 07:48:04 -0400

    volume: use SB_PRINT_ERROR

--- statusbar.c
+++ statusbar.c
@@ -941,7 +941,7 @@ static void *sb_volume_routine(void *thunk)
 #endif
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
-		fprintf(stderr, "Volume routine: Failed to destroy mutex\n");
+		SB_PRINT_ERROR("Failed to destroy mutex");
 	routine->print = SB_FALSE;
 	return NULL;
 }
