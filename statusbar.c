fd54e3fb94e1dcc17fdc4031dd0e627fe8881b1d
Hilde N <snhilde@protonmail.com>
2019-07-11 07:46:47 -0400

    time: use SB_PRINT_ERROR

--- statusbar.c
+++ statusbar.c
@@ -726,7 +726,7 @@ static void *sb_time_routine(void *thunk)
 #endif
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
-		fprintf(stderr, "Time routine: Failed to destroy mutex\n");
+		SB_PRINT_ERROR("Failed to destroy mutex");
 	routine->print = SB_FALSE;
 	return NULL;
 }
