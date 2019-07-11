f23a34171b370b7b55a790edc1d95122f7c6301d
Hilde N <snhilde@protonmail.com>
2019-07-11 07:48:38 -0400

    wifi: use SB_PRINT_ERROR

--- statusbar.c
+++ statusbar.c
@@ -1074,7 +1074,7 @@ static void *sb_wifi_routine(void *thunk)
 #endif
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
-		fprintf(stderr, "Wifi routine: Failed to destroy mutex\n");
+		SB_PRINT_ERROR("Failed to destroy mutex");
 	routine->print = SB_FALSE;
 	return NULL;
 }
