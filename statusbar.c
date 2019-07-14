0b2eb7d8f0770e7e2df05ff8f274ee9f96224393
Hilde N <snhilde@protonmail.com>
2019-07-13 23:25:47 -0400

    load: run loop while routine->print is true

--- statusbar.c
+++ statusbar.c
@@ -472,7 +472,7 @@ static void *sb_load_routine(void *thunk)
 	double             av[3];
 
 	routine->print = SB_TRUE;
-	while(1) {
+	while (routine->print) {
 		SB_START_TIMER;
 
 		fd = fopen(path, "r");
