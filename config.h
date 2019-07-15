281bf12c35560706fec8ab0e869b84cea5314488
Hilde N <snhilde@protonmail.com>
2019-07-15 07:12:16 -0400

    change color to color_normal, add color_warning and color_error

--- config.h
+++ config.h
@@ -2,27 +2,29 @@
 static const struct {
 	enum sb_routine_e  routine;
 	time_t             seconds;
-	const char        *color;
+	const char        *color_normal;
+	const char        *color_warning;
+	const char        *color_error;
 } chosen_routines[] = {
-	/* TOP BAR */
-	{ TODO     , 5      , "#FFFFFF" },
-	{ WEATHER  , 60 * 30, "#FFFFFF" },
-	{ WIFI     , 5      , "#FFFFFF" },
-	{ TIME     , 1      , "#FFFFFF" },
+	/* TOP BAR */       /* normal     warning    error */
+	{ TODO     , 5      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ WEATHER  , 60 * 30, "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ WIFI     , 5      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ TIME     , 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
 
 	/* DELIMITER BETWEEN BARS */
-	{ DELIMITER, 0      , NULL      },
+	{ DELIMITER, 0      , NULL     , NULL     , NULL      },
 
 	/*BOTTOM BAR */
-	{ NETWORK  , 1      , "#FFFFFF" },
-	{ DISK     , 5      , "#FFFFFF" },
-	{ RAM      , 5      , "#FFFFFF" },
-	{ LOAD     , 1      , "#FFFFFF" },
-	{ CPU_USAGE, 1      , "#FFFFFF" },
-	{ CPU_TEMP , 1      , "#FFFFFF" },
-	{ FAN      , 1      , "#FFFFFF" },
-	{ BATTERY  , 30     , "#FFFFFF" },
-	{ VOLUME   , 1      , "#FFFFFF" },
+	{ NETWORK  , 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ DISK     , 5      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ RAM      , 5      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ LOAD     , 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ CPU_USAGE, 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ CPU_TEMP , 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ FAN      , 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ BATTERY  , 30     , "#FFFFFF", "#BB4F2E", "#A1273E" },
+	{ VOLUME   , 1      , "#FFFFFF", "#BB4F2E", "#A1273E" },
 };
 SB_BOOL color_text = SB_TRUE; /* SB_FALSE to not color text, or if status2d patch is not installed */
 
