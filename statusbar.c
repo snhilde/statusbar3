3f573a9064a187dcc6ed1fcd456c83f569e846d6
Hilde N <snhilde@protonmail.com>
2019-07-16 23:13:02 -0400

    weather: change handle to curl

--- statusbar.c
+++ statusbar.c
@@ -985,7 +985,7 @@ static void *sb_weather_routine(void *thunk)
 
 #ifdef BUILD_WEATHER
 	SB_TIMER_VARS;
-	CURL *handle;
+	CURL *curl;
 
 	routine->color = routine->colors.normal;
 	while (routine->print) {
