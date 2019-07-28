e06b643d969bb3fbd2bd2727234be86cf2b1750c
Hilde N <snhilde@protonmail.com>
2019-07-28 10:18:12 -0400

    disk: unlink routine if loop is broken

--- src/statusbar.c
+++ src/statusbar.c
@@ -473,7 +473,7 @@ static void *sb_disk_routine(void *thunk)
 
 	if (pthread_mutex_destroy(&(routine->mutex)) != 0)
 		sb_print_error(routine, "Failed to destroy mutex");
-	routine->run = SB_FALSE;
+	sb_remove_routine(routine);
 	return NULL;
 }
 
