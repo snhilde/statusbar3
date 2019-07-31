fd4e4526ec99305a8f302baa2f63e7ff9cde9ab5
Hilde N <snhilde@protonmail.com>
2019-07-31 07:11:27 -0400

    enhance comment

--- src/statusbar.c
+++ src/statusbar.c
@@ -93,7 +93,8 @@ static SB_BOOL sb_isrgb(const char *color)
 static void sb_leak_check(const char *name)
 {
 #ifdef DEBUG
-	/* If a leak is found, these will halt the program. */
+	/* If a leak is found, these will halt the program.
+ 	 * They must be run in every non-terminating loop. */
 	__lsan_do_leak_check();
 	__lsan_disable();
 	sb_debug(name, "no leaks found");
