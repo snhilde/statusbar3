131fa195e9b81239899153ecc798bee5e1a4c807
Hilde N <snhilde@protonmail.com>
2019-08-04 11:45:34 -0400

    change color pointers to arrays

--- src/statusbar.h
+++ src/statusbar.h
@@ -73,9 +73,9 @@ typedef struct sb_routine {
 	                                   master status bar string to copy. */
 	const char        *color;       /* Font color for each print cycle. */
 	struct {
-		const char    *normal;      /* Font color for normal values. */
-		const char    *warning;     /* Font color when routine is in warning range. */
-		const char    *error;       /* Font color when routine has an error. */
+		const char    *normal[8];   /* Font color for normal values. */
+		const char    *warning[8];  /* Font color when routine is in warning range. */
+		const char    *error[8];    /* Font color when routine has an error. */
 	} colors;
 	pthread_t          thread;      /* Thread assigned to this routine. */
 	pthread_mutex_t    mutex;       /* Mutex assigned to this routine. This will be used to
